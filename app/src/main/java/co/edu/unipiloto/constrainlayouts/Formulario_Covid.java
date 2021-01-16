9fd198d05963556e7780cfbd339869fe0872b434
Diego Manrique <damanrique@outlook.com>
2021-01-16 18:17:23 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/constrainlayouts/Formulario_Covid.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.constrainlayouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Formulario_Covid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario__covid);
+    }
+}
